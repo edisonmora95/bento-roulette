451b963cfe5faf660395bae3762766816eebb46f
Ana Carrión Barbotó <anacbarboto@gmail.com>
2022-05-16 17:50:43 -0500

    roulette form

--- src/components/recommendations-modal/form.js
+++ src/components/recommendations-modal/form.js
@@ -1,5 +1,17 @@
 export default function RecommendationsForm() {
     return (
-        <h1>Form</h1>
+        <div>
+            <form>      
+                <input name="address" type="text" class="feedback-input" placeholder="Current Address" /> 
+                &nbsp;  
+                <label><strong>Price Range</strong></label>
+                <select class="form-select feedback-input" aria-label="Default select example">
+                    <option selected value="$$$">Expensive</option>
+                    <option value="$$">Regular</option>
+                    <option value="$">Cheap</option>
+                </select>
+                <input class="button-59" type="submit" value="SUBMIT"/>
+            </form>
+        </div>
     );
 }

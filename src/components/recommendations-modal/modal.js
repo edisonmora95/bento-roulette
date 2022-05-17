9261fe01d615ee4fad21a120801518a4066b4a6e
Julio Guilindro <julio.guilindro@gmail.com>
2022-05-17 01:52:52 -0400

    Setting width to 1000

--- src/components/recommendations-modal/modal.js
+++ src/components/recommendations-modal/modal.js
@@ -19,7 +19,7 @@ function RecommendationsModal(props) {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 1500,
+        width: 1000,
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
